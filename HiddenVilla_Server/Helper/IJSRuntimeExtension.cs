914a95139cbc9af39a4fe5d1eec1b83a3e8b26ef
Fanavari-7 <Fanavari-7@alborz.com>
2024-08-10 12:11:26 +0330

    Failure & Success notification with diffrent way

---
+++ HiddenVilla_Server/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Server.Helper
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async ValueTask ToasterSuccess(this IJSRuntime JSRuntime,  string message)
+        {
+            await JSRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+
+        public static async ValueTask ToasterError(this IJSRuntime JSRuntime, string message)
+        {
+            await JSRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+    }
+}
