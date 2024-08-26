6c35a96bcd1d963a5e69e36b09d41817c4404df2
Fanavari-7 <Fanavari-7@alborz.com>
2024-08-26 12:57:03 +0330

    add background and DateTime

---
+++ HiddenVilla_Client/Model/ViewModel/HoemVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HiddenVilla_Client.Model.ViewModel
+{
+    public class HoemVM
+    {
+        public DateTime startDate { get; set; } = DateTime.Now;
+        public DateTime EndDate { get; set; }
+        public int NoOFNights { get; set; } = 1;
+    }
+}
