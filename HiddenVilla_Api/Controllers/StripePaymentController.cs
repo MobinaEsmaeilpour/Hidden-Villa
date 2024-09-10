c33d688f4c5dddb4b401dbbaa8ae17b74063dda4
Fanavari-7 <Fanavari-7@alborz.com>
2024-09-10 14:38:57 +0330

    add stripe Models

---
+++ HiddenVilla_Api/Controllers/StripePaymentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HiddenVilla_Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StripePaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
